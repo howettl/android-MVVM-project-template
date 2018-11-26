6471930d52b8d786a42acdd0f77adb7bf94022e3
Lee Howett <lee@fingerfoodstudios.com>
2018-11-26 14:19:44 -0800

    added up button support

--- app/src/main/java/com/howettl/mvvm/ui/MainActivity.kt
+++ app/src/main/java/com/howettl/mvvm/ui/MainActivity.kt
@@ -2,12 +2,29 @@ package com.howettl.mvvm.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.howettl.mvvm.R
 
 class MainActivity: AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController: NavController by lazy {
+        findNavController(R.id.fragment)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
